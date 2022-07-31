a8704d1ea8543060527ccf460ecd088fde07ba77
Marvish <mschandr@uci.edu>
2022-07-31 11:19:17 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+# Marvish Chandra
+
+import re
+
+
+def __init__(self,numEmployees,numCustomers):
+    self.numEmployees = numEmployees
+    self.numCustomers = numCustomers
+
+
+
+def united_States_Postal_Service(self,fname,lname,email,operationalDays,operationalHours):
+    fname = ""
+    refName = re.string("[a-Z]{0,15}$/",fname)
+    print(refName)
+    lname = ""
+    relName = re.string("[a-Z]{0,15}$/",lname)
+    print(relName)
+    email = ""
+    clientEmail = re.string("^(a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.(a-zA-Z]{2,10})$")
+    print(clientEmail)
+
+    services = []
+
+    operationalDays = {"Monday": 0, "Tuesday": 1, "Wednesday": 2, "Thursday": 3, "Friday": 3, "Saturday": 4}
+    operationalHours = [9, 10, 11, 1, 2, 3, 4, 5]
+    daysKeys = d.keys()
+    print(daysKeys)
+    for h in operationalHours:
+        print(h)
+
+
+def localLibrary(fname,lname,email,operationalDays,operationalHours):
+    fname = ""
+    llfName = re.string("[a-Z]{0,15}$/",fname)
+    print(llfName)
+    lname = ""
+    lllname = re.string("[a-Z]{0,15}$/",lname)
+    email = ""
+    llclientEmail = re.string("^[a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,10})$")
+    print(llclientEmail)
+    
+    services = []
+
+
+def foodDistribution(operationalDays,operationalHours):
+
+    services = []
+
+
+def clothesDrive(operationalDays,operationalHours):
+
+    services = []
+
+
+
+def hospitals(fname,lname,operationalDays,operationalHours):
+
+    services = []
+
+
+
+def financialInstitutions(fname,lname,operationalDays,operationalHours):
+
+    services = []
